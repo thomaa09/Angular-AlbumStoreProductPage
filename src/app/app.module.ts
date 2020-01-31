19c78207c933d3ed6915dbf161af641a179fd759
Aubin Thomas <aubin.thomas@enerweb.co.za>
2020-01-31 17:00:06 +0200

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,13 @@ import { ProductDescriptionComponent } from './product-description/product-descr
 import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: 'products', component: ProductListComponent},
+  {path: 'product/:id', component: ProductPageComponent},
+  {path: '', redirectTo: 'products', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
@@ -19,6 +26,7 @@ import { ProductListComponent } from './product-list/product-list.component';
     ProductListComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     HttpModule
